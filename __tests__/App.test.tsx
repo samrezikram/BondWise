ac2c9fe32dfc5b311ebd6a26950cfb00ca5f3637
samrez Ikram <samrezikram@yahoo.com>
2026-03-18 21:29:58 +0400

    Add bond calculation engine

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,15 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import { BondCalculatorScreen } from '../src/screens/BondCalculatorScreen';
+
+jest.mock('react-native-safe-area-context', () => ({
+  SafeAreaProvider: ({ children }: { children: React.ReactNode }) => children,
+  useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
+}));
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+    ReactTestRenderer.create(<BondCalculatorScreen />);
   });
 });
